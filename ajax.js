22f1ebdb93cff9f3070325747af4e8b2bdbda94b
Ed Lerner <CodeTheInternet@users.noreply.github.com>
2017-02-04 09:26:42 -0800

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -15,4 +15,16 @@ var BASEAJAX = {
 var stats = BASEAJAX;
 stats.url += "2/4611686018441896324/2305843009259865633";
 
-$.ajax(stats);
+$.ajax({
+	url: "https://www.bungie.net/Platform/Destiny/2/4611686018441896324/2305843009259865633",
+	method: "GET",
+	beforeSend: function(xhr){
+		xhr.setRequestHeader('X-API-Key', "6bc6d0c9e58a45958f7671e186a080b0");
+	},
+	success: function(res){
+		console.log(res);
+	},
+	error: function(res){
+		console.error(res);
+	}
+});
