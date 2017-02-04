e7219a55553a7766942e32d925a7b7487e2a3344
Ed Lerner <CodeTheInternet@users.noreply.github.com>
2017-02-04 09:42:06 -0800

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -1,6 +1,11 @@
+var MEMBERSHIP_TYPE = "2/";
+var MEMBERSHIP_ID   = "4611686018441896324/";
+var CHARACTER_ID    = "2305843009259865633/";
+
 var BASEAJAX = {
 	url: "https://www.bungie.net/Platform/Destiny/",
 	method: "GET",
+	data: {},
 	beforeSend: function(xhr){
 		xhr.setRequestHeader('X-API-Key', "6bc6d0c9e58a45958f7671e186a080b0");
 	},
@@ -13,19 +18,15 @@ var BASEAJAX = {
 };
 
 var stats = BASEAJAX;
-stats.url += "2/4611686018441896324/2305843009259865633";
+stats.url += "Stats/ActivityHistory/"+MEMBERSHIP_TYPE+MEMBERSHIP_ID+CHARACTER_ID;
+stats.data = {
+	"lc":"en",
+	"fmt":true,
+	"lcin":true",
+	"mode":5,
+	"count":20,
+	"page":0,
+	"definitions":true
+};
 
-$.ajax({
-	url: "https://www.bungie.net/Platform/Destiny/2/4611686018441896324/2305843009259865633",
-	method: "GET",
-	beforeSend: function(xhr){
-		xhr.setRequestHeader('X-API-Key', "6bc6d0c9e58a45958f7671e186a080b0");
-	},
-	success: function(res){
-		console.log(res);
-	},
-	error: function(res){
-		console.error(res.responseText);
-		$('article').text(res.responseText);
-	}
-});
+$.ajax(stats);
