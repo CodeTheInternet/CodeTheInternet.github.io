ed76883d493a39a482c53ce2969358f13d414e0c
Ed Lerner <CodeTheInternet@users.noreply.github.com>
2017-02-04 12:18:20 -0800

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -1,3 +1,4 @@
+var APIKEY          = "6bc6d0c9e58a45958f7671e186a080b0";
 var BASEURL         = "https://www.bungie.net/Platform/Destiny/";
 var MEMBERSHIP_TYPE = "2/";
 var MEMBERSHIP_ID   = "4611686018441896324/";
@@ -6,7 +7,7 @@ var CHARACTER_ID    = "2305843009259865633/";
 function handleResponse(res){
 	if (res.ErrorCode) {
 		var err = res.ErrorCode+": "+res.Message;
-		console.error(err);
+		console.error(res);
 		$('#status').attr('class','error').text(err);
 	} else {
 		console.log(res);
@@ -16,6 +17,7 @@ function handleResponse(res){
 
 $.ajax({
 	url: BASEURL+"Stats/ActivityHistory/"+MEMBERSHIP_TYPE+MEMBERSHIP_ID+CHARACTER_ID,
+	beforeSend: function(xhr){ xhr.setRequestHeader('X-API-Key', APIKEY); },
 	method: "GET",
 	data: {
 		"lc":"en",
@@ -26,9 +28,7 @@ $.ajax({
 		"page":0,
 		"definitions":true
 	},
-	beforeSend: function(xhr){
-		xhr.setRequestHeader('X-API-Key', "6bc6d0c9e58a45958f7671e186a080b0");
-	},
+	
 	success: function(res){
 		handleResponse(res);
 	},
