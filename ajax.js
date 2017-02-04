99edac2bd08fd94d2092d057a2c3c3fd7dbc6f37
Ed Lerner <CodeTheInternet@users.noreply.github.com>
2017-02-04 13:46:26 -0800

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -34,7 +34,7 @@ $.ajax({
 	// url: BASEURL+"Stats/ActivityHistory/"+MEMBERSHIP_TYPE+MEMBERSHIP_ID+CHARACTER_ID,
 	// url: BASEURL+"Stats/AggregateActivityStats/"+MEMBERSHIP_TYPE+MEMBERSHIP_ID+CHARACTER_ID,
 	// url: BASEURL+"Stats/UniqueWeapons/"+MEMBERSHIP_TYPE+MEMBERSHIP_ID+CHARACTER_ID,
-	url: BASEURL+"/Stats/PostGameCarnageReport/"+ACTIVITY_ID,
+	url: BASEURL+"Stats/PostGameCarnageReport/"+ACTIVITY_ID,
 	beforeSend: function(xhr){ xhr.setRequestHeader('X-API-Key', APIKEY); },
 	method: "GET",
 	data: {
